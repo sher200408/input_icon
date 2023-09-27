5c74b633f70f06e4b0ea5d744643535a2eaf5782
shermuhammad shertayev backend <113122559+sher200408@users.noreply.github.com>
2023-09-27 10:13:39 +0500

    Add files via upload

---
+++ Git/mine.js
@@ -0,0 +1,6 @@
+let toggleMenu = document.querySelector('.toggleMenu');
+let navigation = document.querySelector('.navigation');
+
+toggleMenu.onclick = function () {
+    navigation.classList.toggle('active')
+}
